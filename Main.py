3431c49903ca114a60eb114c4088bb6c6dce0e21
Daniel Rivera <danielf.r97@gmail.com>
2019-04-17 12:25:59 -0500

    Funciones de nuevo, abrir,guardar listas.

--- Main.py
+++ Main.py
@@ -1,89 +1,192 @@
-# import os
+import tkinter as tk
+from tkinter import filedialog, messagebox, Menu, StringVar, Entry, Label, Button, Text
 from tkinter.ttk import Treeview
-from tkinter import filedialog, messagebox
-from tkinter import *
+
+import openpyxl
 
 
-def donothing():
-    print("n")
-
-
-def abrir():
-    f = filedialog.askopenfile(mode="r")
-    linea = f.readline()
-    if linea:
-        while linea:
-            if linea[-1] == "\n":
-                linea = linea[:-1]
-            print(linea)
-            contactos.append(linea)
-            linea = f.readline()
-    f.close()
+def new():
+    global savePath
+    savePath = filedialog.asksaveasfilename(filetypes=[("Excel files", "*.xlsx *.xls")])
+    a = open('Config/path.txt', 'w')
+    a.write(savePath)
+    a.close()
+    deleteALl()
 
 
 def guardar():
-    f = filedialog.asksaveasfile(mode='w', defaultextension=".txt")
-    if f is None:  # asksaveasfile return `None` if dialog closed with "cancel".
-        return
-    text2save = str(("Holiiis" + "\n" + "holita"))  # starts from `1.0`, not `0.0`
-    f.write(text2save)
-    f.close()
+    global savePath
+    wb = openpyxl.Workbook()
+    hoja = wb.active
+    hoja.title = "Hoja 1"
+    fila = 1  # Fila donde empezamos
+    for i in tv.get_children():
+        hoja.cell(column=1, row=fila, value=tv.item(i)['text'])
+        hoja.cell(column=2, row=fila, value=tv.item(i)['values'][0])
+        hoja.cell(column=3, row=fila, value=tv.item(i)['values'][1])
+        hoja.cell(column=4, row=fila, value=tv.item(i)['values'][2])
+        hoja.cell(column=5, row=fila, value=tv.item(i)['values'][3])
+        fila += 1
+
+    wb.save(filename=savePath)
 
 
-contactos = []
-
-def agregar():
-    name = StringVar()
-    tel = StringVar()
-    email = StringVar()
-    root2=Tk()
-    root2.geometry("400x400")
-    root2.configure(background = "white")
-    nameBox = Entry(root2, textvariable = name).place(x = 150, y = 60)
-    nameLabel = Label(root2, text = "Nombre: ", bg = "white", fg = "black").place(x = 50, y =60)
-    telBox = Entry(root2, textvariable = tel).place(x = 150, y = 90)
-    telLabel = Label(root2, text = "Teléfono: ", bg = "white", fg = "black").place(x = 50, y =90)
-    emailBox = Entry(root2, textvariable = email).place(x = 150, y = 120)
-    emailLabel = Label(root2, text = "Email: ", bg = "white", fg = "black").place(x = 50, y =120)
-    botonGuardar = Button(root2, text = "Guardar", bg = "#CFD4F0", fg ="black").place(x=150,y = 250)
-    #root.destroy()    
+def add():
+    root2.deiconify()
+    btn.place_forget()
+    btn2.place(x=110, y=230)
 
 
+def edit():
+    if tv.focus():
+
+        selected = tv.item(tv.focus())
+        name.set(selected['text'])
+        lastName.set((selected['values'][0]))
+        tel.set((selected['values'][1]))
+        cel.set((selected['values'][2]))
+        email.set((selected['values'][3]))
+        btn2.place_forget()
+        btn.place(x=110, y=230)
+        root2.deiconify()
+    else:
+        messagebox.showinfo("Error", "Ninguna persona ha sido seleccionada.")
 
 
-def LoadTable():
-    tv.insert('', 'end', text="Danielito", values=("454648",45445,"Danielasd@"))
-    tv.insert('', 'end', text="Karolsita", values=("allalalla",'meh', 'sadk'))
+def addEdit():
+    if (name.get() == "" or lastName.get() == "" or cel.get() == "" or tel.get() == "" or email.get == ""):
+        messagebox.showinfo("Error", "Debe llenar todos los datos.")
+    else:
+        tv.item(tv.focus(), text=name.get(), values=[lastName.get(), tel.get(), cel.get(), email.get()])
+        root2.iconify()
+        btn.place_forget()
+        cleanBox()
 
 
-root = Tk()
-root.title("Otalparo")
-root.geometry("500x500")
-root.configure(background = "white")
+def deleteALl():
+    for i in tv.get_children():
+        global aux
+        aux -= 1
+        tv.delete(i)
+
+
+def delete():
+    if tv.focus():
+        global aux
+        aux -= 1
+        tv.delete(tv.focus())
+
+    else:
+        messagebox.showinfo("Error", "Ninguna persona ha sido seleccionada.")
+
+
+def loadTable():
+    if (name.get() == "" or lastName.get() == "" or cel.get() == "" or tel.get() == "" or email.get == ""):
+        messagebox.showinfo("Error", "Debe llenar todos los datos.")
+    else:
+        global aux
+        aux += 1
+        tv.insert('', 'end', text=name.get(), values=(lastName.get(), tel.get(), cel.get(), email.get()))
+        root2.iconify()
+        cleanBox()
+        print(aux)
+
+
+def cleanBox():
+    name.set("")
+    lastName.set("")
+    tel.set("")
+    cel.set("")
+    email.set("")
+
+
+def abrir():
+    global savePath
+    savePath = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx *.xls")])
+    a = open('Config/path.txt', 'w')
+    a.write(savePath)
+    a.close()
+    deleteALl()
+    global columnas
+    columnas = list(openpyxl.load_workbook(savePath)["Hoja 1"])
+    append()
+
+
+def append():
+    print(tv.get_children())
+    for i in range(len(columnas)):
+        tv.insert('', 'end', text=columnas[i][0].value,
+                  values=(columnas[i][1].value, columnas[i][2].value, columnas[i][3].value, columnas[i][4].value))
+
+
+f = open('Config/path.txt', 'r')
+savePath = f.read()
+f.close()
+columnas = list(openpyxl.load_workbook(savePath)["Hoja 1"])
+print(savePath)
+aux = len(columnas)
+
+root = tk.Tk()
 menubar = Menu(root)
 text = Text(root)
+name = StringVar()
+lastName = StringVar()
+tel = StringVar()
+email = StringVar()
+cel = StringVar()
+
+root.title("Agenda")
+root.geometry("720x300")
+root.configure(background="white")
+
+root2 = tk.Toplevel(root)
+root2.geometry("280x300")
+root2.configure(background="white")
+root2.iconify()
+
 filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="New", command=donothing)
+filemenu.add_command(label="New", command=new)
 filemenu.add_command(label="Open", command=abrir)
 filemenu.add_command(label="Save", command=guardar)
 filemenu.add_separator()
 filemenu.add_command(label="Exit", command=root.quit)
 menubar.add_cascade(label="File", menu=filemenu)
 filemenu.add_separator()
+
+Label(root2, text="AGREGAR CONTACTO", bg="white", fg="black").place(x=80, y=20)
+Entry(root2, textvariable=name).place(x=120, y=60)
+Label(root2, text="Nombres: ", bg="white", fg="black").place(x=30, y=60)
+Entry(root2, textvariable=lastName).place(x=120, y=90)
+Label(root2, text="Apellidos: ", bg="white", fg="black").place(x=30, y=90)
+Entry(root2, textvariable=tel).place(x=120, y=120)
+Label(root2, text="Teléfono: ", bg="white", fg="black").place(x=30, y=120)
+Entry(root2, textvariable=cel).place(x=120, y=150)
+Label(root2, text="Celular: ", bg="white", fg="black").place(x=30, y=150)
+Entry(root2, textvariable=email).place(x=120, y=180)
+Label(root2, text="Email: ", bg="white", fg="black").place(x=30, y=180)
+btn = Button(root2, text="Guardar", command=addEdit, bg="#CFD4F0", fg="black")
+btn2 = Button(root2, text="Guardar", command=loadTable, bg="#CFD4F0", fg="black")
+
 tv = Treeview()
-tv['columns'] = ('Telefono Fijo', 'Celular', 'Correo electronico')
-tv.heading("#0", text='Nombre completo', anchor='w')
-tv.column("#0", anchor="w")
-tv.heading('Telefono Fijo', text='Telefono Fijo')
+tv['columns'] = ('Apellidos', 'Telefono Fijo', 'Celular', 'Correo electronico')
+tv.heading("#0", text='  Nombres', anchor='w')
+tv.column("#0", anchor="w", width=150)
+tv.heading('Apellidos', text='  Apellidos', anchor='w')
+tv.column('Apellidos', anchor="w", width=150)
+tv.heading('Telefono Fijo', text='Teléfono Fijo')
 tv.column('Telefono Fijo', anchor='center', width=100)
 tv.heading('Celular', text='Celular')
-tv.column('Celular', anchor='center', width=100)
-tv.heading('Correo electronico', text='Correo electronico')
-tv.column('Correo electronico', anchor='center', width=100)
-tv.grid(sticky=(N, S, W, E))
-botonAgregar = Button(root, text = "Agregar", command = agregar,
-    bg = "#CFD4F0", fg ="black").place(x=100,y=400)
+tv.column('Celular', anchor='center', width=120)
+tv.heading('Correo electronico', text='Correo electrónico')
+tv.column('Correo electronico', anchor='center', width=180)
+tv.grid(sticky=("N", "S", "W", "E"))
+tv.place(x=10, y=10)
 
-LoadTable()
+append()
+
+Button(root, text="Agregar", command=add, bg="#CFD4F0", fg="black").place(x=100, y=250)
+Button(root, text="Editar", command=edit, bg="#CFD4F0", fg="black").place(x=200, y=250)
+Button(root, text="Eliminar", command=delete, bg="#CFD4F0", fg="black").place(x=300, y=250)
+
 root.config(menu=menubar)
 root.mainloop()
